9fe426798fffa17006a9f2968b5390ab651c8509
Chia Che Chang <midtic0404@gmail.com>
2024-02-01 17:23:49 -0700

    Remove unused declarations and configure Amplify with AWS exports

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
